01e178166c6c7082f8d91763413e38e910e81791
weslley <wesleyyrocha20@gmail.com>
2024-06-01 21:53:44 -0300

    conetando projeto com front end

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const uuid = require('uuid')
+const cors = require('cors');
 
+const port = 3001;
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const users = []
 app.get('/users', (request, response) => {
@@ -52,4 +55,4 @@ if (index < 0) {
   users.splice(index,1)
     return response.status(204).json()
 })
-app.listen(3000)
+app.listen(3001);
